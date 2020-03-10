1886e38e296966086e62eb70926ec13c39359e23
Michael Mazamba <michaelmazamba07@gmail.com>
2020-03-10 11:08:31 -0500

    tipo de datos
    tipo de datos

---
+++ Tipo de datos/src/tipo/de/datos/TipoDeDatos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipo.de.datos;
+
+/**
+ *
+ * @author usuario
+ */
+public class TipoDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    // enteros -32768 a 32767
+    int nuemroEntero  = 0;
+    double numeroDoble = 0.0;
+    char letraChar = 'A';
+    String cadena = "A";
+    boolean VoF = false;
+    VoF = true;
+    boolean VoF1 = true;
+    
+  
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+    }
+    
+}
+
+// comentario de una linea
+/*
+comentario 
+de 
+un 
+parrafo
+*/
+
