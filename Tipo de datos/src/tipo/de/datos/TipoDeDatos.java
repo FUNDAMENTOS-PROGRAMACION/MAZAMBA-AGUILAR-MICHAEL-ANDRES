da2d6a1d5c08ca4d57fb5dcc4a206c8d8ed6d5e5
Michael Mazamba <michaelmazamba07@gmail.com>
2020-03-11 11:41:38 -0500

    avance

--- Tipo de datos/src/tipo/de/datos/TipoDeDatos.java
+++ Tipo de datos/src/tipo/de/datos/TipoDeDatos.java
@@ -30,11 +30,22 @@ public class TipoDeDatos {
     imprimirNombre (); 
     suma2Numeros(8,2);
     System.out.println(suma2Numeros(8,2));
+    
+    
     }
    
   public static void imprimirNombre (){
-    System.out.println("Michael");
-  
+    System.out.println("Michael \n Mazamba"); //salto de linea
+    System.out.println("mi \t chael");
+    String nombre = "Michael";
+    //System.out.println(nombre.length());
+    //System.out.println(nombre.trim());
+    //System.out.println(nombre.toUpperCase());
+    //System.out.println(nombre.toLowerCase());
+    System.out.println(nombre.replace(nombre, "peerro") );
+    System.out.println(nombre );
+    System.out.println(nombre.split("-"));
+    
 }  
   public static int suma2Numeros (int num1, int num2){
       int suma = num1 + num2;
